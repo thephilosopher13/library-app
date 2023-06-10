7058fd8be545c39c492015ef82c9ca62efbce17b
Coloqs <60373194+thephilosopher13@users.noreply.github.com>
2023-06-10 21:42:16 +0800

    create row constructor but only has properties

--- javascript.js
+++ javascript.js
@@ -1,22 +1,60 @@
 let myLibrary = [];
 
-
-//functions for adding and removing new Books from library
-
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-};
-
+// Variables relating to table creation, editing and clearing
 const table = document.getElementById('libraryTable');
+const tbody = table.querySelector('tbody');
+const getRecentlyCreatedBook = myLibrary[myLibrary.length - 1];
+const rowCreator = document.createElement('tr')
+const cellCreator = document.createElement('td');
 
 
+// Variables for searching for a certain object
+var recentlyCreatedBook = books[books.length - 1]
 
 class Library {
-    function addBookToLibrary(newBook) {
-        myLibrary.push(newBook);
-      }
+    addBooktoLibrary(book) {
+        myLibrary.push(book);
+    }
     
+    createCell(property, content) {
+        const newCell = cellCreator.cloneNode();
+        newCell.textContent = content;
+        newCell.classList.add(property);
+        return newCell;
+    }
+
+    // functionality for button to toggle the hasRead property
+    toggleReadStatus(book, row, hasRead) {
+        book.hasRead = !hasRead;
+        newRow.querySelector('.hasRead').textContent = book.hasRead ? 'Yes' : 'No';
+    } 
+
+    addRecentlyCreatedBookToTable(book) {
+        const newRow = rowCreator.cloneNode();
+
+        newRow.appendChild(this.createCell(book.title));
+        newRow.appendChild(this.createCell(book.author));
+        newRow.appendChild(this.createCell(book.pages));
+        newRow.appendChild(this.createCell(book.hasRead ? 'Yes' : 'No'));
+
+        tbody.appendChild(newRow);
+
+    }
+
+
+
+
 }
+
+class Book {
+    constructor(title, author, pages, hasRead) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages
+        this.hasRead = hasRead;
+      }
+};
+
 
 document.getElementById('addBookForm').addEventListener(submit, (e) => {
     e.preventDefault();
@@ -30,6 +68,7 @@ document.getElementById('addBookForm').addEventListener(submit, (e) => {
     var newBook = new addBook(title, author, pages, hasRead);
 
     addBookToLibrary(newBook)
+    addRecentlyCreatedBookToTable(getRecentlyCreatedBook)
 });
 
 
